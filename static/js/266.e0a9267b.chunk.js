c6eecf69d83ec375502fb11375fd5569b3596ab5
FolkinaEkaterina <83020203+FolkinaEkaterina@users.noreply.github.com>
2023-05-29 12:49:49 +0000

    Deploying to gh-pages from @ FolkinaEkaterina/goit-react-hw-08-phonebook@d4d074b1f1afd688653dd8efe4226d6938ffa728 🚀

---
+++ static/js/266.e0a9267b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var t=r(8856),c=r(184);function o(){return(0,c.jsxs)(t.$0,{children:[(0,c.jsx)(t.s2,{children:"phone"}),(0,c.jsx)(t.so,{children:"book"})]})}}}]);
+//# sourceMappingURL=266.e0a9267b.chunk.js.map
